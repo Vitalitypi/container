1a82bec18c41327f3102236898533baa30f11fcf
Vitalitypi <779958365@qq.com>
2024-01-31 23:46:21 +0800

    tets

--- index.js
+++ index.js
@@ -1,30 +1,61 @@
-const express = require('express')
-const bodyParser = require('body-parser')
+const path = require("path");
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
 const { init: initDB, Counter } = require("./db");
-const PORT = process.env.PORT || 80
 
-const app = express()
+const logger = morgan("tiny");
 
-app.use(bodyParser.raw())
-app.use(bodyParser.json({}))
-app.use(bodyParser.urlencoded({ extended: true }))
+const app = express();
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
+app.use(logger);
 
-app.all('/', async (req, res) => {
-  console.log('消息推送', req.body)
-  const { ToUserName, FromUserName, MsgType, Content, CreateTime } = req.body
-  if (MsgType === 'text') {
-    res.send({
-      ToUserName: FromUserName,
-      FromUserName: ToUserName,
-      CreateTime: CreateTime,
-      MsgType: 'text',
-      Content: await Counter.count()
-    })
-  } else {
-    res.send('success')
+// 首页
+app.get("/", async (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+// 更新计数
+app.post("/api/count", async (req, res) => {
+  const { action } = req.body;
+  if (action === "inc") {
+    await Counter.create();
+  } else if (action === "clear") {
+    await Counter.destroy({
+      truncate: true,
+    });
   }
-})
+  res.send({
+    code: 0,
+    data: await Counter.count(),
+  });
+});
 
-app.listen(PORT, function () {
-  console.log(`运行成功，端口：${PORT}`)
-})
+// 获取计数
+app.get("/api/count", async (req, res) => {
+  const result = await Counter.count();
+  res.send({
+    code: 0,
+    data: result,
+  });
+});
+
+// 小程序调用，获取微信 Open ID
+app.get("/api/wx_openid", async (req, res) => {
+  if (req.headers["x-wx-source"]) {
+    res.send(req.headers["x-wx-openid"]);
+  }
+});
+
+const port = process.env.PORT || 80;
+
+async function bootstrap() {
+  await initDB();
+  app.listen(port, () => {
+    console.log("启动成功", port);
+  });
+}
+
+bootstrap();
