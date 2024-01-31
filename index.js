2188d10209a1eb69ca9bfbe80f00a6e09e0e2aeb
Vitalitypi <779958365@qq.com>
2024-01-31 22:24:38 +0800

    reply

--- index.js
+++ index.js
@@ -10,9 +10,16 @@ app.use(bodyParser.json({}))
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.all('/', async (req, res) => {
-  console.log(res)
+  console.log('消息推送', req.body)
   const { ToUserName, FromUserName, MsgType, Content, CreateTime } = req.body
   if (MsgType === 'text') {
+    res.send({
+      ToUserName: FromUserName,
+      FromUserName: ToUserName,
+      CreateTime: CreateTime,
+      MsgType: 'text',
+      Content: '这是回复的消息'
+    })
     if (Content === '回复文字') {
       res.send({
         ToUserName: FromUserName,
